d865ddffcc4996ffded1ea2e79c879da5159613f
Mykola <mikolakovalchukwt@gmail.com>
2016-12-04 23:09:19 +0200

    Added possibility to register for users

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,9 @@ ActiveRecord::Schema.define(version: 20161204202608) do
     t.datetime "created_at",                                  null: false
     t.datetime "updated_at",                                  null: false
     t.string   "role",                   default: "business"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
