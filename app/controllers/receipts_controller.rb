ea6f508b8e8ef6dd5a82400846bc4c4eadb16eb9
Volodymyr.Partyka <volodymyr.partyka@teamvoy.com>
2016-12-07 18:45:39 +0200

    Hotfix

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -1,6 +1,5 @@
 class ReceiptsController < ApplicationController
   def generate_receipt
-    params = { receipt_type: :pdf }
     op = Receipt::Operation::Create.call(params)
 
     send_file op.path_to_receipt
